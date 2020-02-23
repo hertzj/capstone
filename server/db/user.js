147a17b2cba483154dd2d155c6769b5cfee61fad
Jake Hertz <hertzjake@gmail.com>
2020-02-23 18:11:12 -0500

    database start

---
+++ server/db/user.js
@@ -0,0 +1,66 @@
+const Sequelize = require('sequelize');
+const db = require('./database');
+var bcrypt = require('bcrypt');
+
+const { STRING, UUID, UUIDV4 } = Sequelize;
+
+const User = db.define(
+  'user',
+  {
+    id: {
+      type: UUID,
+      defaultValue: UUIDV4,
+      primaryKey: true,
+    },
+    firstName: {
+      type: STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    lastName: {
+      type: STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    City: {
+      type: STRING,
+      allowNull: true,
+    },
+    email: {
+      type: STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+        isEmail: true,
+      },
+    },
+    password: {
+      type: STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+  },
+  {
+    hooks: {
+      beforeCreate: user => {
+        const { password } = user;
+        const saltRounds = 10;
+        const salt = bcrypt.genSaltSync(saltRounds);
+        const hash = bcrypt.hashSync(password, salt);
+        user.password = hash;
+      },
+    },
+  }
+);
+
+User.prototype.isPasswordValid = function(password) {
+  return bcrypt.compareSync(password, this.password);
+};
+
+module.exports = User;
